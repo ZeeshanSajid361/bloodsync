e8c26170e3851bc0c0331abd5a607e184805bddf
ZeeshanSajid361 <zeeshansajid361@gmail.com>
2026-08-11 14:30:08 +0500

    Minimal serverless handler test

--- client/api/index.js
+++ client/api/index.js
@@ -1,29 +1,5 @@
 'use strict';
 
-// Handle OPTIONS preflight IMMEDIATELY before loading anything else.
-module.exports = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token,X-Requested-With,Accept,Accept-Version,Content-Length,Content-MD5,Content-Type,Date,X-Api-Version,Authorization'
-  );
-
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-
-  try {
-    const { connectDB } = require('./src/config/db');
-    const app = require('./src/app');
-    await connectDB();
-    return app(req, res);
-  } catch (err) {
-    console.error('[serverless] Fatal error:', err);
-    return res.status(500).json({
-      success: false,
-      message: 'Internal serverless error',
-      error: err.message,
-    });
-  }
+module.exports = (req, res) => {
+  res.status(200).json({ success: true, message: 'Vercel serverless function working!' });
 };
